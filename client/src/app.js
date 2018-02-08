ef2a99383301ef8210b8e8a4b388a19fa7789681
nate <nate.a.willard@gmail.com>
2018-02-08 15:14:47 -0800

    stuff

--- client/src/app.js
+++ client/src/app.js
@@ -3,7 +3,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import Navbar from './components/navbar';
+
+class App extends React.Component {
+  render() {
+    return (
+      <Navbar />
+    );
+  }
+}
+
 ReactDOM.render(
-  <h1>Hi</h1>,
+  <App />,
   document.getElementById('root')
 );
